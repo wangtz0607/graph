07d41622a06d97441caa6badd9f5514e0f37b18f
wtz <wangtz0607@outlook.com>
2024-06-28 00:54:13 +0800

    Support direct comparison of `Infinity` with other types

--- include/graph/Infinity.h
+++ include/graph/Infinity.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <compare>
 #include <limits>
 
 namespace graph {
@@ -17,5 +18,25 @@ struct Infinity {
         return kInfinity<T>;
     }
 };
+
+template <typename T>
+auto operator==(T lhs, Infinity rhs) {
+    return lhs == static_cast<T>(rhs);
+}
+
+template <typename T>
+auto operator==(Infinity lhs, T rhs) {
+    return static_cast<T>(lhs) == rhs;
+}
+
+template <typename T>
+auto operator<=>(T lhs, Infinity rhs) {
+    return lhs <=> static_cast<T>(rhs);
+}
+
+template <typename T>
+auto operator<=>(Infinity lhs, T rhs) {
+    return static_cast<T>(lhs) <=> rhs;
+}
 
 } // namespace graph
